bae4d2dbe8982bdf31ecd3f70c908a2fcf55315e
Damián Martínez <p82maavd@uco.es>
2023-10-02 22:02:28 +0200

    Update basic ArffReader implementation

--- datasets/arffReaderTest.py
+++ datasets/arffReaderTest.py
@@ -1,17 +1,32 @@
-class MLMIArff:
+class MIMLArff:
     def __init__(self) -> None:
         self.name = "undefined"
         self.attributes = []
-        self.data = []
+        self.data = dict()
     
     def setName(self, name):
         self.name = name
     def getName(self):
         return self.name
+    
+    def setAttributes(self, attributes):
+        self.attributes=attributes
+    def getAttributes(self):
+        return self.attributes
+    
+    def addBag(self,key,values,labels):
+        self.data[key]=(values,labels)
+    
+    def showArff(self):
+        #TODO: Improve this to table style
+        for keys,values in self.data.items():
+            print(keys)
+            print(values)
 
 
 def arffMIMLReader(file):
 
+    arff = MIMLArff()
     arff_file = open(file)
     count=0
     for line in arff_file:
@@ -32,12 +47,23 @@ def arffMIMLReader(file):
                     
                     print("Key: ", key)
                     print("Values ", values)
+                    valueslist=[]
+                    for v in values:
+                        valueslist.append(tuple([float(i) for i in v.split(',')]))
                     print("Labels: ", labels)
+                    
+                    arff.addBag(key,tuple(valueslist),tuple([int(i) for i in labels.split(',')]))
                             #Cuando se llegue a data, se añade el id del bag a un diccionario y los values de cada clave 
                             #sera una tupla de 2 elementos, una tupla con tuplas de los distintos values y otra tupla con las etiquetas
-                            #value: (((3,5,6,7),(1,4,5,7)),(1,0,1))
-                            #incluso diccionario aparte para stats
+                            #key: (((3,5,6,7),(1,4,5,7)),(1,0,1))
+                            #TODO: incluso diccionario aparte para stats
+    
+    return arff
 
-arffMIMLReader("toy.arff")
+toy=arffMIMLReader("toy.arff")
+toy.showArff()
 
+#TODO: Datos en los datasets diferentes, en birds hay un espacio entre valores
 
+birds=arffMIMLReader("miml_birds.arff")
+birds.showArff()
