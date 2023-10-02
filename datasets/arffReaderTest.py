67e759e5fe64cccc8ef07b4ea0e30b0a2f2ff4d8
Damián Martínez <p82maavd@uco.es>
2023-10-02 20:35:57 +0200

    Arff miml reader basic implementation

---
+++ datasets/arffReaderTest.py
@@ -0,0 +1,43 @@
+class MLMIArff:
+    def __init__(self) -> None:
+        self.name = "undefined"
+        self.attributes = []
+        self.data = []
+    
+    def setName(self, name):
+        self.name = name
+    def getName(self):
+        return self.name
+
+
+def arffMIMLReader(file):
+
+    arff_file = open(file)
+    count=0
+    for line in arff_file:
+            count+=1
+            line=line.lstrip()
+            if line=="":
+                continue
+            
+            if not (line.startswith("@")):
+                if not (line.startswith("%")):
+                    line=line.strip("\n")
+                    #line=line.split(',')
+                    #print(line)
+                    key = line[0:line.find(",")]
+                    line=line[line.find(",")+2:]
+                    labels=line[line.find("\"",2)+2:]
+                    values = line[:line.find("\"",2)].split("\\n")
+                    
+                    print("Key: ", key)
+                    print("Values ", values)
+                    print("Labels: ", labels)
+                            #Cuando se llegue a data, se añade el id del bag a un diccionario y los values de cada clave 
+                            #sera una tupla de 2 elementos, una tupla con tuplas de los distintos values y otra tupla con las etiquetas
+                            #value: (((3,5,6,7),(1,4,5,7)),(1,0,1))
+                            #incluso diccionario aparte para stats
+
+arffMIMLReader("toy.arff")
+
+
