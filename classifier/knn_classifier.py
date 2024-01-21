e80f699d80d1c2d6ef98507d4779fc2c2554af0c
Damián Martínez <p82maavd@uco.es>
2024-01-21 21:29:12 +0100

    update

--- classifier/knn_classifier.py
+++ classifier/knn_classifier.py
@@ -1,8 +1,5 @@
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-
-import numpy as np
 
 import classifier.abstract_classifier
 
@@ -14,16 +11,13 @@ class KNNClassifier(classifier.abstract_classifier.AbstractClassifier):
         self.model = None
 
     def train(self, training_data, training_labels):
-        # Training logic using KNN
         self.model = KNeighborsClassifier(n_neighbors=self.k)
         self.model.fit(training_data, training_labels)
 
     def predict(self, test_data):
-        # Prediction logic using KNN
         return self.model.predict(test_data)
 
     def evaluate(self, test_data, test_labels):
-        # Evaluation logic using KNN
         predictions = self.predict(test_data)
         accuracy = accuracy_score(test_labels, predictions)
         return accuracy
--- data/bag.py
+++ data/bag.py
@@ -5,15 +5,58 @@ class Bag:
 
     def __init__(self, instance):
         #Crear numpy ndarray 3d con una instancia
+        self.data = []
+        self.key = ""
 
     def __init__(self, instances):
         #Crear numpy ndarray 3d con varias instancias
         pass
 
+    def get_number_instances(self):
+        """
+        Get numbers of instances of a bag
+
+        Parameters
+        ----------
+        key : string
+            Key of the bag
+
+        Returns
+        ----------
+        numbers of instances: int
+            Numbers of instances of a bag
+
+        """
+        return len(self.data)
+
     def get_instance(self, index):
+        """
+
+        Parameters
+        ----------
+
+        index : int
+            Index of the instance in the bag
+
+        Returns
+        -------
+        instance : tuple of ndarrays
+            Tuple with attribute values and label of the instance
+
+        """
         pass
 
     def add_instance(self, instance):
+        """
+
+        Parameters
+        ----------
+        key : string
+            Key of the bag
+        values : ndarray
+            Values of the instance to be inserted
+
+        """
         pass
 
     def add_attribute(self, name, position, value=0):
@@ -22,6 +65,20 @@ class Bag:
         pass
 
     def set_attribute(self, index, value):
+        """
+        Update value from attributes
+
+        Parameters
+        ----------
+        key : string
+            Bag key of the dataset
+
+        attribute: string
+            Attribute of the dataset
+
+        value: float
+            New value for the update
+        """
         pass
 
     def set_attribute(self, name, value):
