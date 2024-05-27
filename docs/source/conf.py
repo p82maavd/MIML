4b18a51cf3baeb637076552746d54bde5bce499d
Damián Martínez <p82maavd@uco.es>
2024-05-27 02:01:04 +0200

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,6 @@ sys.path.insert(0, os.path.abspath('../../'))
 sys.path.insert(0, os.path.abspath('../../src'))
 sys.path.insert(0, os.path.abspath('../../src/'))
 
-print("HOLA")
 
 project = 'miml'
 copyright = '2024, Damian'
