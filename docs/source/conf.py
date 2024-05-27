2aa0575adcbec390a71e205155db2df94e2dd864
Damián Martínez <p82maavd@uco.es>
2024-05-27 03:03:04 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,10 +9,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../miml'))
-print(os.getcwd())
-os.system("cd ..")
-print(os.listdir())
+sys.path.insert(0, os.path.abspath('../src/miml'))
+
 
 project = 'miml'
 copyright = '2024, Damian'
