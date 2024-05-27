53bad55353d80fafd0960beb1421b24cf99b66c7
Damián Martínez <p82maavd@uco.es>
2024-05-27 03:07:51 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,6 +11,7 @@ import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../src/'))
 sys.path.insert(0, os.path.abspath('../../../src/'))
+os.system("tree /home/runner/work /F")
 
 
 project = 'miml'
